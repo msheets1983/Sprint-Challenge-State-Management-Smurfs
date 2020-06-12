0cf6497ec7d5c0751ba5bebd55c683fbad869378
Michael Sheets <michaelsheets@MacBook-Pro.local>
2020-06-12 11:42:28 -0500

    completed actions

--- smurfs/src/components/SmurfsForm.js
+++ smurfs/src/components/SmurfsForm.js
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { postSmurfs } from "../actions";
 
-const SmurfsForm = () => {
+const SmurfsForm = (props) => {
+  const [smurf, setSmurf] = useState({
+    name: "",
+    age: "",
+    height: "",
+  });
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    props.postSmurf(smurf);
+    document.getElementById("SmurfsForm").reset();
+  };
+
+  const inputHandler = (e) => {
+    e.preventDefault();
+    setSmurf({ ...smurf, [e.target.name]: e.target.value });
+  };
+
   return (
-    <div>
+    <div className="form">
+      <form id="smurfForm">
+        <input
+          type="text"
+          name="name"
+          label="name"
+          placeholder="Smurf Name"
+          value={props.name}
+          onChange={inputHandler}
+          className="input"
+        />
+        <input
+          type="text"
+          name="age"
+          label="age"
+          placeholder="Smurf Age"
+          value={props.age}
+          onChange={inputHandler}
+          className="input"
+        />
+        <input
+          type="text"
+          name="height"
+          label="height"
+          placeholder="Smurf Height"
+          value={props.height}
+          onChange={inputHandler}
+          className="input"
+        />
+        <button onClick={submitHandler} className="btn">
+          Add New Smurf
+        </button>
+      </form>
       <h1>Smurfs Form</h1>
     </div>
   );
 };
 
-export default SmurfsForm;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps, { postSmurfs })(SmurfsForm);
--- smurfs/src/components/SmurfsList.js
+++ smurfs/src/components/SmurfsList.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SmurfsCard from "./SmurfsCard";
+import { connect } from "react-redux";
+import { fetchSmurfs } from "../actions";
 
-const SmurfsList = () => {
+const SmurfsList = (props) => {
+  console.log(props);
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
+
   return (
     <div>
       <h2>Smurfs List</h2>
-      <SmurfsCard />
+      <div className="card-container">
+        {props.smurf.map((smurf) => (
+          <SmurfsCard key={smurf.id} smurf={smurf} />
+        ))}
+      </div>
     </div>
   );
 };
 
-export default SmurfsList;
+const mapStateToProps = (state) => {
+  return {
+    smurfs: state.smurfs,
+    isFetching: state.isFetching,
+    error: state.error,
+  };
+};
+
+export default connect(mapStateToProps, { fetchSmurfs })(SmurfsList);
